e5d5a28bf3e1124ee3ff042b5e355a7d01d7b711
ljuelch <leonardo.juelch@eqs.com>
2024-07-02 14:52:03 +0200

    new: Base URL Service init

---
+++ src/app/services/base-url.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseUrlService {
+  endpointBaseUrl = `https://www.swapi.tech/api`;
+  constructor(private http: HttpClient) {}
+
+  getEndpoint(): Observable<unknown> {
+    return this.http.get(this.endpointBaseUrl);
+  }
+}
