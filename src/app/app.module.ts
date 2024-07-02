dc0e6ec2f32d750e7cbcc5908ffc67db5261346f
ljuelch <leonardo.juelch@eqs.com>
2024-07-02 02:56:48 +0200

    new: implememt death star after init page load, adjust anims

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ResultsComponent } from './wiki-container/results/results.component';
 import { SwordComponent } from './sword/sword.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SaberSwitchComponent } from './saber-switch/saber-switch.component';
+import { NgOptimizedImage } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,8 @@ import { SaberSwitchComponent } from './saber-switch/saber-switch.component';
     BrowserModule,
     AppRoutingModule,
     NgxParticlesModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -2,24 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { IParticlesProps, NgParticlesService } from '@tsparticles/angular';
 import { loadFull } from 'tsparticles';
 import { Container } from '@tsparticles/engine';
-import { particlesOptions} from '../../assets/particles-options';
+import { particlesOptions } from '../../assets/particles-options';
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
-  styleUrl: './background.component.scss'
+  styleUrls: ['./background.component.scss']
 })
 export class BackgroundComponent implements OnInit {
   constructor(private readonly ngParticlesService: NgParticlesService) {}
+  particlesOptions: IParticlesProps = particlesOptions;
 
   id = "tsparticles";
-  particlesOptions: IParticlesProps = particlesOptions;
+  showSpaceship = false;
+  shakeSpaceship = false;
+  disappearSpaceship = false;
+
   ngOnInit(): void {
     this.ngParticlesService.init(async (engine) => {
       await loadFull(engine);
     });
+    setTimeout(() => {
+      this.showSpaceship = true;
+      setTimeout(() => {
+        this.shakeSpaceship = true;
+        setTimeout(() => {
+          this.shakeSpaceship = false;
+          setTimeout(() => {
+            this.disappearSpaceship = true;
+          }, 30);
+        }, 1000);
+      }, 1500);
+    }, 6100);
   }
 
-  particlesLoaded(container: Container): void {
-  }
+  particlesLoaded(container: Container): void {}
 }
